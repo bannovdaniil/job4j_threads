cd3355be1466981e1ba823ab9662bf5ab0adae87
bannovdaniil <baddan@mail.ru>
2024-01-06 19:03:12 +0300

    8. Скачивание файла с ограничением. [#144271 #470727]

--- src/main/java/ru/job4j/concurrent/Wget.java
+++ src/main/java/ru/job4j/concurrent/Wget.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.net.URL;
 
 /**
@@ -14,19 +16,22 @@ import java.net.URL;
  * 2. В метод main добавьте валидацию входных параметров.
  * 2. Загрузите код в репозиторий и оставьте ссылку на коммит.
  * <p>
+ * Файлы качаются в папку DOWNLOAD_PATH, если папки нет она создается.
+ * имя файла берется из URL если, имени нет тогда index.html
  * Алгоритм паузы потока:
  * 1. качаем пакет
- * 2. находим скорость с которой скачали блок. (количество байт / время)
- * 3. если скорость меньше или такаяже как заданная, переходим в 1.
- * 4. считаем время задержки потока: (расчетное количество - заданное количество) / скорость скачивания. U = s / t ; t = s / U;
- * 4.1. заданное количество: speed т.к. время = 1 ; U = s / 1; => s = U;
- * 4.2. расчетное количество: s = U * t;  =>  s = U * 1; s = U;
- * 5. ожидаем расчитаное время
- * 6. переходим в 1.
+ * 2. проверяем количество скаченного привысило заданную величину?
+ * 3. если ДА
+ * 3.1. Расчитываем разницу времени, (1_000.0d * receivedSize / speed) - receivedTime / 1_000_000.0d;
+ * 3.2. Проверяем потраченное время больше чем 1 миллисекунда?
+ * 3.3. ожидаем расчитаное время
+ * 3.4. обнуляем счетчики.
+ * 5. перехоиди в 1.
  */
 public class Wget implements Runnable {
     private final String url;
     private final int speed;
+    private static final String DOWNLOAD_PATH = "download" + File.separator;
 
     /**
      * @param url   - ссылка для скачиваемого файла.
@@ -39,42 +44,73 @@ public class Wget implements Runnable {
 
     @Override
     public void run() {
-        File file = new File("tmp.xml");
+        File file = getFileFromURL(url);
         long startAt = System.currentTimeMillis();
 
         try (InputStream input = new URL(url).openStream();
              FileOutputStream output = new FileOutputStream(file)
         ) {
             System.out.println("Open connection: " + (System.currentTimeMillis() - startAt) + " ms");
-            byte[] dataBuffer = new byte[512];
+            byte[] dataBuffer = new byte[1024];
             int bytesRead;
 
+            long receivedSize = 0L;
+            long size = 0L;
+            long receivedTime = 0L;
+            long startDownloadTime = System.nanoTime();
             while (!Thread.currentThread().isInterrupted() && (bytesRead = input.read(dataBuffer, 0, dataBuffer.length)) != -1) {
                 long downloadAt = System.nanoTime();
 
                 output.write(dataBuffer, 0, bytesRead);
 
-                long operationTime = (System.nanoTime() - downloadAt);
-                long downloadSpeed = bytesRead * 1_000_000 / operationTime;
-                long sleepTime = 0;
-                if (downloadSpeed > speed) {
-                    sleepTime = (downloadSpeed - speed) / speed;
+                receivedSize += bytesRead;
+                size += bytesRead;
+                receivedTime += (System.nanoTime() - downloadAt);
+
+                System.out.printf("\rLoading: Read %d bytes : %d nano. ", receivedSize, receivedTime);
+                if (receivedSize >= speed) {
+                    sleepProcess(receivedTime, receivedSize);
+                    receivedSize = 0;
+                    receivedTime = 0;
                 }
-                System.out.printf("Loading: Read %d bytes : %d nano. Sleep time: %d%n", bytesRead, operationTime, sleepTime);
-                sleepProcess(sleepTime);
             }
+            sleepProcess(receivedTime, receivedSize);
+            long time = System.nanoTime() - startDownloadTime;
+            System.out.printf("%nSize: %d, Time: %d, Download speed: %.4f%n", size, time, 1_000_000_000.0d * size / time);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private void sleepProcess(long sleepTime) {
+    private void sleepProcess(long receivedTime, long receivedSize) {
+        double sleepTime = (1_000.0d * receivedSize / speed) - receivedTime / 1_000_000.0d;
         if (sleepTime > 0) {
+            System.out.printf("Sleep time: %f ", sleepTime);
             try {
-                Thread.sleep(sleepTime);
+                Thread.sleep((long) sleepTime);
             } catch (InterruptedException e) {
                 Thread.currentThread().interrupt();
             }
+        }
+    }
+
+    private File getFileFromURL(String url) {
+        String fileName = "file.tmp";
+        try {
+            fileName = new File(new URI(url).getPath()).getName();
+            if (fileName.isEmpty()) {
+                fileName = "index.html";
+            }
+        } catch (URISyntaxException e) {
+        }
+        createPathFolder(DOWNLOAD_PATH);
+        return new File(DOWNLOAD_PATH + fileName);
+    }
+
+    private void createPathFolder(String downloadPath) {
+        File path = new File(downloadPath);
+        if (!path.exists()) {
+            path.mkdirs();
         }
     }
 
